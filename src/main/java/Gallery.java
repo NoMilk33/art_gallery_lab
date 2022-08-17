38aedfaac16edb2f5b10edc13717b4f62a00baf2
Hanqing Huang <h.hanqing@yahoo.com>
2022-08-17 15:42:55 +0100

    gallery constructor

--- src/main/java/Gallery.java
+++ src/main/java/Gallery.java
@@ -5,4 +5,10 @@ public class Gallery {
     private String name;
     private int till;
     private ArrayList<Artwork> artwork;
+    
+    public Gallery(String name, int till) {
+    this.artwork= new ArrayList();
+    this.name= name;
+    this.till= till;
+    }
 }
