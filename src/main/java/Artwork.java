52ddba63f784a0442b998ee75cf6ec9a4b71bbc5
Hanqing Huang <h.hanqing@yahoo.com>
2022-08-17 15:36:16 +0100

    add artwork constructor

--- src/main/java/Artwork.java
+++ src/main/java/Artwork.java
@@ -3,4 +3,12 @@ public class Artwork {
     private Artist artist;
     private float price;
     private float nft;
+
+    public Artwork(String title, Artist artist, float price, float nft){
+        this.title = title;
+        this.artist = artist;
+        this.price = price;
+        this.nft = nft;
+
+    }
 }
